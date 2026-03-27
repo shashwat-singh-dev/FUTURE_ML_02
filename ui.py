8c20d48514dc397a4f5dcc2220a597def94b7e02
Shashwat Singh <shashwat3210dl@gmail.com>
2026-03-27 20:13:03 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -16,27 +16,21 @@ if st.button("Predict"):
         st.warning("Please enter an issue first")
 
     else:
-           with st.spinner("Processing..."):
-    try:
-        response = requests.post(
-            API_URL,
-            json={"text": text},
-            timeout=60
-        )
-
-        response.raise_for_status()
-        data = response.json()
-
-        # 👇 YEH SAB TRY KE ANDAR HI RAHEGA
-        st.markdown("### 🎯 Prediction Result")
-
-        col1, col2 = st.columns(2)
-
-        with col1:
-            st.success(f"📂 Category\n\n**{data['category']}**")
-
-        with col2:
-            st.info(f"⚡ Priority\n\n**{data['priority']}**")
-
-    except Exception as e:
-        st.error(f"Error: {e}")
+        with st.spinner("Processing..."):
+            try:
+                response = requests.post(
+                    API_URL,
+                    json={"text": text},
+                    timeout=60
+                )
+        
+                response.raise_for_status()
+                data = response.json()
+                st.markdown("### 🎯 Prediction Result")
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.success(f"📂 Category\n\n**{data['category']}**")
+                with col2:
+                    st.info(f"⚡Priority\n\n**{data['priority']}**")
+            except Exception as e:
+                st.error(f"Error: {e}")
